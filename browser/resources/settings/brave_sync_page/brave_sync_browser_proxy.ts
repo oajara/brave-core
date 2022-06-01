296007ede83385b5c86376068f619e3746c839ba
AlexeyBarabash <alexey@brave.com>
2022-06-01 19:00:28 +0300

    Introduced BraveSyncHandler::HandlePermanentlyDeleteAccount

--- browser/resources/settings/brave_sync_page/brave_sync_browser_proxy.ts
+++ browser/resources/settings/brave_sync_page/brave_sync_browser_proxy.ts
@@ -41,6 +41,9 @@ export class BraveSyncBrowserProxy {
   deleteDevice(deviceId: string): Promise<boolean> {
     return sendWithPromise('SyncDeleteDevice', deviceId);
   }
+  permanentlyDeleteSyncAccount() {
+    return sendWithPromise('SyncPermanentlyDeleteAccount');
+  }
   static getInstance() {
     return instance || (instance = new BraveSyncBrowserProxy())
   }
